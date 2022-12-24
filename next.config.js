d21a5c23c2b6f7a5dea2de02f0fcaba4295d98d4
Klaatu98 <arabson1998@gmail.com>
2022-12-24 23:31:44 +0400

    asdd env

--- next.config.js
+++ next.config.js
@@ -4,4 +4,7 @@ module.exports = {
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')],
     },
+    env: {
+        BACK_URL: process.env.BACK_URL,
+    }
 }
--- utils/HandleFetchData.ts
+++ utils/HandleFetchData.ts
@@ -1,10 +1,12 @@
 const API_URL = "http://localhost:5000";
 
 export const handleFetchData = async (formData: FormData) => {
-    const res = await fetch(`${API_URL}/api/generate`, {
+    const res = await fetch(`${process.env.BACK_URL}/api/generate`, {
         method: 'POST',
         body: formData,
     })
+
+    console.log(process.env)
 
     return res
 }
